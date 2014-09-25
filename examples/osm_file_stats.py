2af99106a3df126523bf74159a6296bd5c027568
Sarah Hoffmann <lonvia@denofr.de>
2014-09-25 20:29:36 +0200

    add counting example

---
+++ examples/osm_file_stats.py
@@ -0,0 +1,28 @@
+import osmium as o
+import sys
+
+class FileStatsHandler(o.SimpleHandler):
+    def __init__(self):
+        o.SimpleHandler.__init__(self)
+        self.nodes = 0
+        self.ways = 0
+        self.rels = 0
+
+    def node(self, n):
+        pass self.nodes += 1
+
+    def way(self, w):
+        pass self.ways += 1
+
+    def relation(self, r):
+        pass self.rels += 1
+
+
+fd = o.Reader(sys.argv[1])
+h = FileStatsHandler()
+
+o.apply(fd, h)
+
+print "Nodes:", h.nodes
+print "Ways:", h.ways
+print "Relations:", h.rels
