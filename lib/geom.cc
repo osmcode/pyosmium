44e302ae9bedd0ac4b07eaa530dee01719fda017
Sarah Hoffmann <lonvia@denofr.de>
2015-04-09 21:40:03 +0200

    export use_nodes and direction enums and use in create_lienstring
    fixes #4

--- lib/geom.cc
+++ lib/geom.cc
@@ -17,6 +17,16 @@ BOOST_PYTHON_MODULE(_geom)
     using namespace boost::python;
     docstring_options doc_options(true, true, false);
 
+    enum_<osmium::geom::use_nodes>("use_nodes")
+        .value("UNIQUE", osmium::geom::use_nodes::unique)
+        .value("ALL", osmium::geom::use_nodes::all)
+    ;
+
+    enum_<osmium::geom::direction>("direction")
+        .value("BACKWARD", osmium::geom::direction::backward)
+        .value("FORWARD", osmium::geom::direction::forward)
+    ;
+
     def("haversine_distance", static_cast<double (*)(const osmium::WayNodeList&)>(&osmium::geom::haversine::distance),
         arg("list"),
         "Compute the distance using the Haversine algorithm which takes the "
@@ -39,10 +49,14 @@ BOOST_PYTHON_MODULE(_geom)
              (arg("self"), arg("ref")),
              "Create a point geometry from a :py:class:`osmium.osm.NodeRef`.")
         .def("create_linestring", static_cast<std::string (WKBFactory::*)(const osmium::WayNodeList&, osmium::geom::use_nodes, osmium::geom::direction)>(&WKBFactory::create_linestring),
-             (arg("self"), arg("list"), arg("use_nodes")="unique", arg("direction")="forward" ),
+             (arg("self"), arg("list"),
+              arg("use_nodes")=osmium::geom::use_nodes::unique,
+              arg("direction")=osmium::geom::direction::forward),
              "Create a LineString geometry from a :py:class:`osmium.osm.WayNodeList`.")
         .def("create_linestring", static_cast<std::string (WKBFactory::*)(const osmium::Way&, osmium::geom::use_nodes, osmium::geom::direction)>(&WKBFactory::create_linestring),
-             (arg("self"), arg("way"), arg("use_nodes")="unique", arg("direction")="forward" ),
+             (arg("self"), arg("way"),
+              arg("use_nodes")=osmium::geom::use_nodes::unique,
+              arg("direction")=osmium::geom::direction::forward),
              "Create a LineString geometry from a :py:class:`osmium.osm.Way`.")
         .def("create_multipolygon", &WKBFactory::create_multipolygon,
              (arg("self"), arg("area")),
