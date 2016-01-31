bee3bac09718f3a2c07a5efbbf38f35c53205823
Sarah Hoffmann <lonvia@denofr.de>
2016-01-31 22:47:07 +0100

    add simple convert example

---
+++ examples/convert.py
@@ -0,0 +1,37 @@
+"""
+Converts a file from one format to another.
+
+This example shows how to write objects to a file.
+"""
+
+import osmium as o
+
+import sys
+
+class Convert(o.SimpleHandler):
+
+    def __init__(self, writer):
+        o.SimpleHandler.__init__(self)
+        self.writer = writer
+
+    def node(self, n):
+        self.writer.add_node(n)
+
+    def way(self, w):
+        self.writer.add_way(w)
+
+    def relation(self, r):
+        self.writer.add_relation(r)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: python convert.py <infile> <outfile>")
+        sys.exit(-1)
+
+    writer = o.SimpleWriter(sys.argv[2])
+    handler = Convert(writer)
+
+    handler.apply_file(sys.argv[1])
+
+    writer.close()
+
