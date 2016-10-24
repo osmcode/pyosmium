69529edc58918559ca96cce62d9e0b4840b492ab
Sarah Hoffmann <lonvia@denofr.de>
2016-10-24 21:41:26 +0200

    setup source distribution for publishing on Pypi

---
+++ README.rst
@@ -0,0 +1,55 @@
+========
+pyosmium
+========
+
+This library provides Python bindings for the `Libosmium`_ C++
+library, a library for working with OpenStreetMap data in a fast and flexible
+manner.
+
+.. _Libosmium: https://github.com/osmcode/libosmium
+
+Installation
+============
+
+pyosmium can be installed with pip:
+
+    pip install osmium
+
+The Pypi source package already comes bundled with a matching version of
+libosmium. The library uses `Boost.Python`_ to create the C++ bindings.
+On Debian/Ubuntu install `libboost-python-dev`. 
+On OS X run `brew install boost-python` or `brew install boost-python --with-python3`
+depending on which python version you want to use. You can also (re)install both.
+
+Python >= 2.7 is supported but a version >= 3.3 is strongly recommended.
+
+Other dependencies are expat, libz, and libbz2. See `Libosmium dependencies`_
+for required packages for various distributions.
+
+.. _Boost.Python: http://www.boost.org/doc/libs/1_56_0/libs/python/doc/index.html
+.. _Libosmium dependencies: https://github.com/osmcode/libosmium/wiki/Libosmium-dependencies
+
+Documentation
+=============
+
+The documentation for the latest release is available at
+`osmcode.org`_.
+
+.. _osmcode.org: http://docs.osmcode.org/pyosmium/latest
+
+Examples
+========
+
+The package contains an `example` directory with small examples on how to use
+the library. They are mostly ports of the examples in Libosmium and
+osmium-contrib.
+
+Fineprint
+=========
+
+Pyosmium is available under the BSD 2-Clause License. See LICENSE.TXT.
+
+The source code can be found on `GitHub`_. There is also the issue tracker
+for reporting bugs.
+
+.. _GitHub: https://github.com/osmcode/pyosmium
--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
-from distutils.core import setup, Extension
-from os import environ
+from setuptools import setup, Extension
+from setuptools.command.sdist import sdist as orig_sdist
+from subprocess import call
 from sys import version_info as pyversion, platform as osplatform
 from ctypes.util import find_library
 
+osmium_version = '2.9.0'
+version = '2.9.0'
 includes = []
 libs = []
 libdirs = []
+
+class My_sdist(orig_sdist):
+
+    def make_release_tree(self, base_dir, files):
+        orig_sdist.make_release_tree(self, base_dir, files)
+        # checkout libosmium in the required version
+        print("Downloading and adding libosmium sources")
+        call('cd ' + base_dir + ' && wget -O - -q https://github.com/osmcode/libosmium/archive/v2.9.0.tar.gz | tar xz', shell=True)
+
+
 
 ## boost dependencies
 includes.append('/usr/include')
@@ -31,6 +44,7 @@ else:
     raise Exception("Cannot find boost_python library")
 
 ### osmium dependencies
+includes.append('libosmium-' + osmium_version + '/include')
 includes.append('../libosmium/include')
 osmium_libs = ('expat', 'pthread', 'z', 'bz2')
 libs.extend(osmium_libs)
@@ -69,10 +83,38 @@ for ext in ('osm', 'replication'):
          ))
     packages.append('osmium.%s' % ext)
 
-setup (name = 'pyosmium',
-       version = '2.9.0',
-       description = 'Provides python bindings for libosmium.',
+classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: C++",
+        ]
+
+descfile=open('README.rst')
+long_description = descfile.read()
+descfile.close()
+
+setup (name = 'osmium',
+       version = version,
+       description = 'Python bindings for libosmium, the data processing library for OSM data',
+       long_description=long_description,
+       author='Sarah Hoffmann',
+       author_email='lonvia@denofr.de',
+       maintainer='Sarah Hoffmann',
+       maintainer_email='lonvia@denofr.de',
+       download_url='https://github.com/osmcode/pyosmium',
+       url='http://osmcode.org/pyosmium',
+       keywords=["OSM", "OpenStreetMap", "Osmium"],
+       license='BSD',
+       classifiers=classifiers,
        packages = packages,
+       cmdclass={'sdist' : My_sdist },
        ext_modules = extensions)
 
 
