96fefc6ef4ef028a2722f1d783086d519eeb02c6
Sarah Hoffmann <lonvia@denofr.de>
2016-11-16 10:34:49 +0100

    introduce global version.py and improve setup error reporting
    version.py now globally defines the packages versions and libosmium
    dependency. It is used by setup.py and Sphinx docs.
    
    setup.py now checks for the existence of libosmium headers before
    adding custom include paths.
    
    Custom locations for libosmium and boost can be passed to setup.py
    via LIBOSMIUM_PREFIX and BOOST_PREFIX environment variables. The
    former is checked for correctness, the latter not.

---
+++ osmium/version.py
@@ -0,0 +1,11 @@
+"""
+Version information.
+"""
+
+# the major version
+pyosmium_major = '2.9'
+# current release (Pip version)
+pyosmium_release = '2.9.0'
+
+# libosmium version shipped with the Pip release
+libosmium_version = '2.9.0'
--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools.command.sdist import sdist as orig_sdist
 from subprocess import call
 from sys import version_info as pyversion, platform as osplatform
 from ctypes.util import find_library
+import os
 
-osmium_version = '2.9.0'
-version = '2.9.0'
 includes = []
 libs = []
 libdirs = []
@@ -15,14 +14,31 @@ class My_sdist(orig_sdist):
     def make_release_tree(self, base_dir, files):
         orig_sdist.make_release_tree(self, base_dir, files)
         # checkout libosmium in the required version
-        print("Downloading and adding libosmium sources")
-        call('cd ' + base_dir + ' && wget -O - -q https://github.com/osmcode/libosmium/archive/v2.9.0.tar.gz | tar xz', shell=True)
+        tarball = 'https://github.com/osmcode/libosmium/archive/v%s.tar.gz' % libosmium_version
+        print("Downloading and adding libosmium sources from", tarball)
+        call('cd %s && wget -O - -q %s | tar xz' % (base_dir, tarball), shell=True)
 
+def get_versions():
+    """ Read the version file.
 
+        The file cannot be directly imported because it is not installed
+        yet.
+    """
+    version_py = os.path.join(os.path.split(__file__)[0], "osmium/version.py")
+    v = {}
+    with open(version_py) as version_file:
+        # Execute the code in version.py.
+        exec(compile(version_file.read(), version_py, 'exec'), v)
+
+    return v['pyosmium_release'], v['libosmium_version']
+
+pyosmium_release, libosmium_version = get_versions()
 
 ## boost dependencies
-includes.append('/usr/include')
-if osplatform in ["linux", "linux2"]:
+includes.append(os.path.join(os.environ.get('BOOST_PREFIX', '/usr'), 'include'))
+if 'BOOST_PREFIX' in os.environ:
+    libdirs.append(os.path.join(os.environ['BOOST_PREFIX'], 'lib'))
+elif osplatform in ["linux", "linux2"]:
     libdirs.append('/usr/lib/x86_64-linux-gnu/')
 
 # try to find the boost library matching the python version
@@ -44,8 +60,23 @@ else:
     raise Exception("Cannot find boost_python library")
 
 ### osmium dependencies
-includes.append('libosmium-' + osmium_version + '/include')
-includes.append('../libosmium/include')
+osmium_prefixes = [ 'libosmium-' + libosmium_version, '../libosmium' ]
+if 'LIBOSMIUM_PREFIX' in os.environ:
+    lo_version_h = os.path.join(os.environ['LIBOSMIUM_PREFIX'],
+                                'include/osmium/version.hpp')
+    if not os.path.isfile(lo_version_h):
+        raise RuntimeError("LIBOSMIUM_PREFIX is set but no libosmium was found in '%s'" % os.environ['LIBOSMIUM_PREFIX'])
+    includes.insert(0, os.path.join(os.environ['LIBOSMIUM_PREFIX'], 'include'))
+else:
+    # default search paths for libosmium
+    for prefix in [ 'libosmium-' + libosmium_version, '../libosmium' ]:
+        if os.path.isfile(os.path.join(prefix, 'include/osmium/version.hpp')):
+            print("libosmium found in '%s'" % prefix)
+            includes.insert(0, os.path.join(prefix, 'include'))
+            break
+    else:
+        print("Using global libosmium.")
+
 osmium_libs = ('expat', 'pthread', 'z', 'bz2')
 libs.extend(osmium_libs)
 
@@ -101,7 +132,7 @@ long_description = descfile.read()
 descfile.close()
 
 setup (name = 'osmium',
-       version = version,
+       version = pyosmium_release,
        description = 'Python bindings for libosmium, the data processing library for OSM data',
        long_description=long_description,
        author='Sarah Hoffmann',
