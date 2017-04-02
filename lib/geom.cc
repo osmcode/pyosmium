62cd6a5005e402ef1383be1b8bf7a4a1397ca93a
Sarah Hoffmann <lonvia@denofr.de>
2017-04-02 23:33:12 +0200

    add WKT and GeoJSON factories

--- lib/geom.cc
+++ lib/geom.cc
@@ -3,6 +3,8 @@
 #include <osmium/geom/haversine.hpp>
 #include <osmium/geom/factory.hpp>
 #include <osmium/geom/wkb.hpp>
+#include <osmium/geom/wkt.hpp>
+#include <osmium/geom/geojson.hpp>
 
 class WKBFactory : public osmium::geom::WKBFactory<> {
 
@@ -11,6 +13,9 @@ public:
     : osmium::geom::WKBFactory<>(osmium::geom::wkb_type::wkb, osmium::geom::out_type::hex)
     {}
 };
+
+using WKTFactory = osmium::geom::WKTFactory<>;
+using GeoJSONFactory = osmium::geom::GeoJSONFactory<>;
 
 BOOST_PYTHON_MODULE(geom)
 {
@@ -62,4 +67,65 @@ BOOST_PYTHON_MODULE(geom)
              (arg("self"), arg("area")),
              "Create a MultiPolygon geometry from a :py:class:`osmium.osm.Area`.")
     ;
+
+    class_<WKTFactory>("WKTFactory",
+        "Factory that creates WKT from osmium geometries.")
+        .add_property("epsg", &WKTFactory::epsg,
+                      "(read-only) EPSG number of the output geometry.")
+        .add_property("proj_string", &WKTFactory::proj_string,
+                      "(read-only) projection string of the output geometry.")
+        .def("create_point", static_cast<std::string (WKTFactory::*)(const osmium::Location&) const>(&WKTFactory::create_point),
+             (arg("self"), arg("location")),
+             "Create a point geometry from a :py:class:`osmium.osm.Location`.")
+        .def("create_point", static_cast<std::string (WKTFactory::*)(const osmium::Node&)>(&WKTFactory::create_point),
+             (arg("self"), arg("node")),
+             "Create a point geometry from a :py:class:`osmium.osm.Node`.")
+        .def("create_point", static_cast<std::string (WKTFactory::*)(const osmium::NodeRef&)>(&WKTFactory::create_point),
+             (arg("self"), arg("ref")),
+             "Create a point geometry from a :py:class:`osmium.osm.NodeRef`.")
+        .def("create_linestring", static_cast<std::string (WKTFactory::*)(const osmium::WayNodeList&, osmium::geom::use_nodes, osmium::geom::direction)>(&WKTFactory::create_linestring),
+             (arg("self"), arg("list"),
+              arg("use_nodes")=osmium::geom::use_nodes::unique,
+              arg("direction")=osmium::geom::direction::forward),
+             "Create a LineString geometry from a :py:class:`osmium.osm.WayNodeList`.")
+        .def("create_linestring", static_cast<std::string (WKTFactory::*)(const osmium::Way&, osmium::geom::use_nodes, osmium::geom::direction)>(&WKTFactory::create_linestring),
+             (arg("self"), arg("way"),
+              arg("use_nodes")=osmium::geom::use_nodes::unique,
+              arg("direction")=osmium::geom::direction::forward),
+             "Create a LineString geometry from a :py:class:`osmium.osm.Way`.")
+        .def("create_multipolygon", &WKTFactory::create_multipolygon,
+             (arg("self"), arg("area")),
+             "Create a MultiPolygon geometry from a :py:class:`osmium.osm.Area`.")
+    ;
+
+    class_<GeoJSONFactory>("GeoJSONFactory",
+        "Factory that creates GeoJSON geometries from osmium geometries.")
+        .add_property("epsg", &GeoJSONFactory::epsg,
+                      "(read-only) EPSG number of the output geometry.")
+        .add_property("proj_string", &GeoJSONFactory::proj_string,
+                      "(read-only) projection string of the output geometry.")
+        .def("create_point", static_cast<std::string (GeoJSONFactory::*)(const osmium::Location&) const>(&GeoJSONFactory::create_point),
+             (arg("self"), arg("location")),
+             "Create a point geometry from a :py:class:`osmium.osm.Location`.")
+        .def("create_point", static_cast<std::string (GeoJSONFactory::*)(const osmium::Node&)>(&GeoJSONFactory::create_point),
+             (arg("self"), arg("node")),
+             "Create a point geometry from a :py:class:`osmium.osm.Node`.")
+        .def("create_point", static_cast<std::string (GeoJSONFactory::*)(const osmium::NodeRef&)>(&GeoJSONFactory::create_point),
+             (arg("self"), arg("ref")),
+             "Create a point geometry from a :py:class:`osmium.osm.NodeRef`.")
+        .def("create_linestring", static_cast<std::string (GeoJSONFactory::*)(const osmium::WayNodeList&, osmium::geom::use_nodes, osmium::geom::direction)>(&GeoJSONFactory::create_linestring),
+             (arg("self"), arg("list"),
+              arg("use_nodes")=osmium::geom::use_nodes::unique,
+              arg("direction")=osmium::geom::direction::forward),
+             "Create a LineString geometry from a :py:class:`osmium.osm.WayNodeList`.")
+        .def("create_linestring", static_cast<std::string (GeoJSONFactory::*)(const osmium::Way&, osmium::geom::use_nodes, osmium::geom::direction)>(&GeoJSONFactory::create_linestring),
+             (arg("self"), arg("way"),
+              arg("use_nodes")=osmium::geom::use_nodes::unique,
+              arg("direction")=osmium::geom::direction::forward),
+             "Create a LineString geometry from a :py:class:`osmium.osm.Way`.")
+        .def("create_multipolygon", &GeoJSONFactory::create_multipolygon,
+             (arg("self"), arg("area")),
+             "Create a MultiPolygon geometry from a :py:class:`osmium.osm.Area`.")
+    ;
+
 }
