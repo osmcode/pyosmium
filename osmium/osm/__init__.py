73ae2cf87ded64699a11ff66c48123daf71e495e
Sarah Hoffmann <lonvia@denofr.de>
2017-04-06 23:56:27 +0200

    add documentation for replace function

--- osmium/osm/__init__.py
+++ osmium/osm/__init__.py
@@ -2,13 +2,25 @@ from ._osm import *
 import osmium.osm.mutable
 
 def create_mutable_node(node, **args):
+    """ Create a mutable node replacing the properties given in the
+        named parameters. Note that this function only creates a shallow
+        copy which is still bound to the scope of the original object.
+    """
     return osmium.osm.mutable.Node(base=node, **args)
 
-def create_mutable_way(node, **args):
-    return osmium.osm.mutable.Way(base=node, **args)
+def create_mutable_way(way, **args):
+    """ Create a mutable way replacing the properties given in the
+        named parameters. Note that this function only creates a shallow
+        copy which is still bound to the scope of the original object.
+    """
+    return osmium.osm.mutable.Way(base=way, **args)
 
-def create_mutable_relation(node, **args):
-    return osmium.osm.mutable.Relation(base=node, **args)
+def create_mutable_relation(rel, **args):
+    """ Create a mutable relation replacing the properties given in the
+        named parameters. Note that this function only creates a shallow
+        copy which is still bound to the scope of the original object.
+    """
+    return osmium.osm.mutable.Relation(base=rel, **args)
 
 Node.replace = create_mutable_node
 Way.replace = create_mutable_way
