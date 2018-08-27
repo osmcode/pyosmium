8f0815534a16077a049f038e648c70b95cfc62f3
Wiktor Niesiobędzki <github@vink.pl>
2018-08-27 22:49:04 +0200

    Update annotations to 2.14.3

--- src/osmium/replication/server.pyi
+++ src/osmium/replication/server.pyi
@@ -1,4 +1,5 @@
 import logging
+from http.client import HTTPResponse
 
 import typing
 from typing import Optional
@@ -19,13 +20,20 @@ class ReplicationServer:
 
     def __init__(self, url: str, diff_type: str = ...) -> None: ...
 
+    def open_url(self, url: str) -> HTTPResponse: ...
+
     def collect_diffs(self, start_id: Sequence, max_size: int = ...) -> DownloadResult: ...
 
     def apply_diffs(self, handler: BaseHandler, start_id: Sequence, max_size: int = ..., idx: str = ...,
                     simplify: bool = ...) -> Optional[Sequence]: ...
 
-    def apply_diffs_to_file(self, infile: str, outfile: str, start_id: Sequence, max_size: int = ...,
-                            set_replication_header: bool = ...) -> Optional[typing.Tuple[Sequence, Sequence]]: ...
+    def apply_diffs_to_file(self,
+                            infile: str,
+                            outfile: str,
+                            start_id: Sequence,
+                            max_size: int = ...,
+                            set_replication_header: bool = ...,
+                            extra_headers: dict = ...) -> Optional[typing.Tuple[Sequence, Sequence]]: ...
 
     def timestamp_to_sequence(self, timestamp: Timestamp, balanced_search: bool = ...) -> Optional[Sequence]: ...
 
