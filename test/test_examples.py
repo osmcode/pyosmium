0d79fa26ec0a0d44c07f4c000bb4ecb0f8cb4081
Sarah Hoffmann <lonvia@denofr.de>
2019-03-02 19:45:55 +0100

    add test for examples

---
+++ test/test_examples.py
@@ -0,0 +1,69 @@
+"""
+Tests for all examples.
+"""
+from nose.tools import *
+import unittest
+import sys
+from helpers import load_script
+from os import path as osp
+
+TEST_FILE='example-test.pbf'
+TEST_DIFF='example-test.osc.gz'
+
+try:
+    from cStringIO import StringIO
+except:
+    from io import StringIO
+
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stdout = self._stdout
+
+def load_example(name):
+    return load_script(osp.join(osp.realpath(__file__),
+                                "..", "..", "examples", name + ".py"))
+
+def test_amenity_list():
+    script = load_example("amenity_list")
+
+    with Capturing() as output:
+        eq_(0, script['main'](TEST_FILE))
+
+    eq_(835, len(output))
+    eq_('1.535245 42.556681 parking', output[0].strip())
+    eq_('1.570729 42.529562 parking         Aparcament Comunal', output[-1].strip())
+
+
+def test_osm_diff_stats():
+    script = load_example("osm_diff_stats")
+
+    with Capturing() as output:
+        eq_(0, script['main'](TEST_DIFF))
+
+    eq_(9, len(output))
+    eq_('Nodes added: 305', output[0])
+    eq_('Nodes modified: 192', output[1])
+    eq_('Nodes deleted: 20', output[2])
+    eq_('Ways added: 31', output[3])
+    eq_('Ways modified: 93', output[4])
+    eq_('Ways deleted: 0', output[5])
+    eq_('Relations added: 0', output[6])
+    eq_('Relations modified: 0', output[7])
+    eq_('Relations deleted: 0', output[8])
+
+def test_osm_file_stats():
+    script = load_example("osm_file_stats")
+
+    with Capturing() as output:
+        eq_(0, script['main'](TEST_FILE))
+
+    eq_(3, len(output))
+    eq_('Nodes: 211100', output[0])
+    eq_('Ways: 10315', output[1])
+    eq_('Relations: 244', output[2])
