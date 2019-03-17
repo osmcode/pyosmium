2c91429df2a0fda2c2f93e088c5fe543c402452d
Wiktor Niesiobędzki <github@vink.pl>
2019-03-17 22:42:55 +0100

    Tests for areas from multipolygon relations and boundary relations

--- test/test_osm.py
+++ test/test_osm.py
@@ -148,6 +148,85 @@ class TestAreaFromWayAttributes(HandlerTestBase, unittest.TestCase):
             assert_true(oring.ends_have_same_location())
             assert_equals(len(list(n.inner_rings(oring))), 0)
 
+class TestAreaFromMultipolygonRelation(HandlerTestBase, unittest.TestCase):
+    data = [osmobj('N', id=1, lat=0, lon=0),
+            osmobj('N', id=2, lat=0, lon=1),
+            osmobj('N', id=3, lat=1, lon=0),
+            osmobj('W', id=23, version=5, changeset=58674, uid=42,
+                   timestamp='2014-01-31T06:23:35Z', user='anonymous',
+                   nodes = [1, 2, 3], tags = {}),
+            osmobj('W', id=24, version=5, changeset=58674, uid=42,
+                   timestamp='2014-01-31T06:23:35Z', user='anonymous',
+                   nodes=[3, 1], tags={}),
+            osmobj('R', id=1, version=3, changeset=7654, uid=42, timestamp='2014-01-31T06:23:35Z', user='Anon',
+                   members=[('way', 23, 'outer'), ('way', 24, 'outer')], tags={'type': 'multipolygon'}),
+            ]
+
+    class Handler(o.SimpleHandler):
+        def area(self, n):
+            assert_equals(n.id, 3)
+            assert_equals(n.deleted, False)
+            assert_equals(n.visible, True)
+            assert_equals(n.version, 3)
+            assert_equals(n.changeset, 7654)
+            assert_equals(n.uid, 42)
+            assert_equals(n.user_is_anonymous(), False)
+            assert_equals(n.timestamp, mkdate(2014, 1, 31, 6, 23, 35))
+            assert_equals(n.user, 'Anon')
+            assert_equals(n.positive_id(), 3)
+            assert_equals(n.orig_id(), 1)
+            assert_equals(n.from_way(), False)
+            assert_equals(n.is_multipolygon(), True)
+            assert_equals(n.num_rings(), (1, 0))
+            assert_equals(len(list(n.outer_rings())), 1)
+            oring = list(n.outer_rings())[0]
+            assert_equals(len(list(oring)), 4)
+            assert_equals(set((1,2,3)), set([x.ref for x in oring]))
+            assert_true(oring.is_closed())
+            assert_true(oring.ends_have_same_id())
+            assert_true(oring.ends_have_same_location())
+            assert_equals(len(list(n.inner_rings(oring))), 0)
+
+class TestAreaFromBoundaryRelation(HandlerTestBase, unittest.TestCase):
+    data = [osmobj('N', id=1, lat=0, lon=0),
+            osmobj('N', id=2, lat=0, lon=1),
+            osmobj('N', id=3, lat=1, lon=0),
+            osmobj('W', id=23, version=5, changeset=58674, uid=42,
+                   timestamp='2014-01-31T06:23:35Z', user='anonymous',
+                   nodes=[1, 2, 3], tags = {}),
+            osmobj('W', id=24, version=5, changeset=58674, uid=42,
+                   timestamp='2014-01-31T06:23:35Z', user='anonymous',
+                   nodes=[3, 1], tags={}),
+            osmobj('R', id=1, version=3, changeset=7654, uid=42, timestamp='2014-01-31T06:23:35Z', user='Anon',
+                   members=[('way', 23, 'outer'), ('way', 24, 'outer')], tags={'type': 'boundary'}),
+            ]
+
+    class Handler(o.SimpleHandler):
+        def area(self, n):
+            assert_equals(n.id, 3)
+            assert_equals(n.deleted, False)
+            assert_equals(n.visible, True)
+            assert_equals(n.version, 3)
+            assert_equals(n.changeset, 7654)
+            assert_equals(n.uid, 42)
+            assert_equals(n.user_is_anonymous(), False)
+            assert_equals(n.timestamp, mkdate(2014, 1, 31, 6, 23, 35))
+            assert_equals(n.user, 'Anon')
+            assert_equals(n.positive_id(), 3)
+            assert_equals(n.orig_id(), 1)
+            assert_equals(n.from_way(), False)
+            assert_equals(n.is_multipolygon(), True)
+            assert_equals(n.num_rings(), (1, 0))
+            assert_equals(len(list(n.outer_rings())), 1)
+            oring = list(n.outer_rings())[0]
+            assert_equals(len(list(oring)), 4)
+            assert_equals(set((1,2,3)), set([x.ref for x in oring]))
+            assert_true(oring.is_closed())
+            assert_true(oring.ends_have_same_id())
+            assert_true(oring.ends_have_same_location())
+            assert_equals(len(list(n.inner_rings(oring))), 0)
+
+
 class TestChangesetAttributes(HandlerTestBase, unittest.TestCase):
     data = [osmobj('C', id=34, created_at="2005-04-09T19:54:13Z",
                 num_changes=2, closed_at="2005-04-09T20:54:39Z",
