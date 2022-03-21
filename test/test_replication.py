5af8a16d509468eb12aa6c15d6d58a3c56c2f613
Sarah Hoffmann <lonvia@denofr.de>
2022-03-21 11:17:00 +0100

    switch replication tests to pytest monkeypatch
    Also introduces tests for customizing opne_url().

--- test/test_replication.py
+++ test/test_replication.py
@@ -11,12 +11,6 @@ import pytest
 
 from helpers import mkdate, CountingHandler
 
-
-try:
-    from unittest.mock import MagicMock, patch
-except ImportError:
-    from mock import MagicMock, patch
-
 import osmium as o
 import osmium.replication.server as rserv
 import osmium.replication
@@ -30,13 +24,6 @@ class RequestsResponses(BytesIO):
     def iter_lines(self):
        return self.readlines()
 
-class UrllibMock(MagicMock):
-
-    def set_result(self, s):
-        self.return_value = RequestsResponses(dedent(s).encode())
-
-    def set_script(self, files):
-        self.side_effect = [RequestsResponses(dedent(s).encode()) for s in files]
 
 @pytest.mark.parametrize("inp,outp", [
         (None,      'https://text.org/state.txt'),
@@ -51,6 +38,7 @@ def test_get_state_url(inp, outp):
 
     assert outp == svr.get_state_url(inp)
 
+
 @pytest.mark.parametrize("inp,outp", [
         (1,         'https://who.is/me//000/000/001.osc.gz'),
         (500,       'https://who.is/me//000/000/500.osc.gz'),
@@ -62,274 +50,6 @@ def test_get_diff_url(inp, outp):
 
     assert outp, svr.get_diff_url(inp)
 
-@patch('osmium.replication.server.requests.Session.get', new_callable=UrllibMock)
-def test_get_state_valid(mock):
-    mock.set_result("""\
-        #Sat Aug 26 11:04:04 UTC 2017
-        txnMaxQueried=1219304113
-        sequenceNumber=2594669
-        timestamp=2017-08-26T11\\:04\\:02Z
-        txnReadyList=
-        txnMax=1219304113
-        txnActiveList=1219303583,1219304054,1219304104""")
-
-    res = rserv.ReplicationServer("https://test.io").get_state_info()
-
-    assert res is not None
-    assert res.timestamp == mkdate(2017, 8, 26, 11, 4, 2)
-    assert res.sequence == 2594669
-
-    assert mock.call_count == 1
-
-@patch('osmium.replication.server.requests.Session.get', new_callable=UrllibMock)
-def test_get_state_sequence_cut(mock):
-    mock.set_script(("""\
-        #Sat Aug 26 11:04:04 UTC 2017
-        txnMaxQueried=1219304113
-        sequenceNumber=259""",
-        """\
-        #Sat Aug 26 11:04:04 UTC 2017
-        txnMaxQueried=1219304113
-        sequenceNumber=2594669
-        timestamp=2017-08-26T11\\:04\\:02Z"""))
-
-    res = rserv.ReplicationServer("https://test.io").get_state_info()
-
-    assert res is not None
-    assert res.timestamp == mkdate(2017, 8, 26, 11, 4, 2)
-    assert res.sequence == 2594669
-
-    assert mock.call_count == 2
-
-@patch('osmium.replication.server.requests.Session.get', new_callable=UrllibMock)
-def test_get_state_date_cut(mock):
-    mock.set_script(("""\
-        #Sat Aug 26 11:04:04 UTC 2017
-        txnMaxQueried=1219304113
-        sequenceNumber=2594669
-        timestamp=2017-08-2""",
-        """\
-        #Sat Aug 26 11:04:04 UTC 2017
-        txnMaxQueried=1219304113
-        sequenceNumber=2594669
-        timestamp=2017-08-26T11\\:04\\:02Z"""))
-
-    res = rserv.ReplicationServer("https://test.io").get_state_info()
-
-    assert res is not None
-    assert res.timestamp == mkdate(2017, 8, 26, 11, 4, 2)
-    assert res.sequence == 2594669
-
-    assert mock.call_count == 2
-
-@patch('osmium.replication.server.requests.Session.get', new_callable=UrllibMock)
-def test_get_state_timestamp_cut(mock):
-    mock.set_script(("""\
-        #Sat Aug 26 11:04:04 UTC 2017
-        txnMaxQueried=1219304113
-        sequenceNumber=2594669
-        timestamp=""",
-        """\
-        #Sat Aug 26 11:04:04 UTC 2017
-        txnMaxQueried=1219304113
-        sequenceNumber=2594669
-        timestamp=2017-08-26T11\\:04\\:02Z"""))
-
-    res = rserv.ReplicationServer("https://test.io").get_state_info()
-
-    assert res is not None
-    assert res.timestamp == mkdate(2017, 8, 26, 11, 4, 2)
-    assert res.sequence == 2594669
-
-    assert mock.call_count == 2
-
-@patch('osmium.replication.server.requests.Session.get', new_callable=UrllibMock)
-def test_get_state_too_many_retries(mock):
-    mock.set_script(("""\
-        #Sat Aug 26 11:04:04 UTC 2017
-        txnMaxQueried=1219304113
-        sequenceNumber=2594669
-        timestamp=""",
-        """\
-        #Sat Aug 26 11:04:04 UTC 2017
-        txnMaxQueried=1219304113
-        sequenceNumber=2594669
-        timestamp=""",
-        """\
-        #Sat Aug 26 11:04:04 UTC 2017
-        txnMaxQueried=1219304113
-        sequenceNumber=2594669
-        timestamp=""",
-        """\
-        #Sat Aug 26 11:04:04 UTC 2017
-        txnMaxQueried=1219304113
-        sequenceNumber=2594669
-        timestamp=2017-08-26T11\\:04\\:02Z"""))
-
-    res = rserv.ReplicationServer("https://test.io").get_state_info()
-
-    assert res is None
-
-    assert mock.call_count == 3
-
-
-
-@patch('osmium.replication.server.requests.Session.get', new_callable=UrllibMock)
-def test_get_state_server_timeout(mock):
-    mock.side_effect = URLError(reason='Mock')
-
-    svr = rserv.ReplicationServer("https://test.io")
-    assert svr.get_state_info() is None
-
-@patch('osmium.replication.server.requests.Session.get', new_callable=UrllibMock)
-def test_apply_diffs_count(mock):
-    mock.set_script(("""\
-        sequenceNumber=100
-        timestamp=2017-08-26T11\\:04\\:02Z
-    """, """
-        n1
-        w1
-        r1
-    """))
-    svr = rserv.ReplicationServer("https://test.io", "opl")
-
-    h = CountingHandler()
-    assert 100 == svr.apply_diffs(h, 100, 10000)
-
-    assert h.counts == [1, 1, 1, 0]
-
-@patch('osmium.replication.server.requests.Session.get', new_callable=UrllibMock)
-def test_apply_diffs_without_simplify(mock):
-    mock.set_script(("""\
-        sequenceNumber=100
-        timestamp=2017-08-26T11\\:04\\:02Z
-    """, """
-        n1 v23
-        n1 v24
-        w1
-        r1
-    """))
-    svr = rserv.ReplicationServer("https://test.io", "opl")
-
-    h = CountingHandler()
-    assert 100 == svr.apply_diffs(h, 100, 10000, simplify=False)
-    assert [2, 1, 1, 0] == h.counts
-
-@patch('osmium.replication.server.requests.Session.get', new_callable=UrllibMock)
-def test_apply_diffs_with_simplify(mock):
-    mock.set_script(("""\
-        sequenceNumber=100
-        timestamp=2017-08-26T11\\:04\\:02Z
-    """, """
-        n1 v23
-        n1 v24
-        w1
-        r1
-    """))
-    svr = rserv.ReplicationServer("https://test.io", "opl")
-
-    h = CountingHandler()
-    assert 100 == svr.apply_diffs(h, 100, 10000, simplify=True)
-    assert [1, 1, 1, 0] == h.counts
-
-@patch('osmium.replication.server.requests.Session.get', new_callable=UrllibMock)
-def test_apply_with_location(mock):
-    mock.set_script(("""\
-        sequenceNumber=100
-        timestamp=2017-08-26T11\\:04\\:02Z
-    """, """
-        n1 x10.0 y23.0
-        w1 Nn1,n2
-    """))
-    svr = rserv.ReplicationServer("https://test.io", "opl")
-
-    class Handler(CountingHandler):
-        def way(self, w):
-            self.counts[1] += 1
-            assert 2 == len(w.nodes)
-            assert 1 == w.nodes[0].ref
-            assert 10 == w.nodes[0].location.lon
-            assert 23 == w.nodes[0].location.lat
-            assert 2 == w.nodes[1].ref
-            assert not w.nodes[1].location.valid()
-
-    h = Handler()
-    assert 100 == svr.apply_diffs(h, 100, 10000, idx="flex_mem")
-
-    assert h.counts == [1, 1, 0, 0]
-
-
-
-@patch('osmium.replication.server.requests.Session.get', new_callable=UrllibMock)
-def test_apply_reader_without_simplify(mock):
-    mock.set_script(("""\
-        sequenceNumber=100
-        timestamp=2017-08-26T11\\:04\\:02Z
-    """, """
-        n1 v23
-        n1 v24
-        w1
-        r1
-    """))
-    svr = rserv.ReplicationServer("https://test.io", "opl")
-
-    h = CountingHandler()
-
-    diffs = svr.collect_diffs(100, 100000)
-    assert diffs is not None
-
-    diffs.reader.apply(h, simplify=False)
-    assert [2, 1, 1, 0] == h.counts
-
-@patch('osmium.replication.server.requests.Session.get', new_callable=UrllibMock)
-def test_apply_reader_with_simplify(mock):
-    mock.set_script(("""\
-        sequenceNumber=100
-        timestamp=2017-08-26T11\\:04\\:02Z
-    """, """
-        n1 v23
-        n1 v24
-        w1
-        r1
-    """))
-    svr = rserv.ReplicationServer("https://test.io", "opl")
-
-    h = CountingHandler()
-    diffs = svr.collect_diffs(100, 100000)
-    assert diffs is not None
-
-    diffs.reader.apply(h, simplify=True)
-    assert [1, 1, 1, 0] == h.counts
-
-@patch('osmium.replication.server.requests.Session.get', new_callable=UrllibMock)
-def test_apply_reader_with_location(mock):
-    mock.set_script(("""\
-        sequenceNumber=100
-        timestamp=2017-08-26T11\\:04\\:02Z
-    """, """
-        n1 x10.0 y23.0
-        w1 Nn1,n2
-    """))
-    svr = rserv.ReplicationServer("https://test.io", "opl")
-
-    class Handler(CountingHandler):
-        def way(self, w):
-            self.counts[1] += 1
-            assert 2 == len(w.nodes)
-            assert 1 == w.nodes[0].ref
-            assert 10 == w.nodes[0].location.lon
-            assert 23 == w.nodes[0].location.lat
-            assert 2 == w.nodes[1].ref
-            assert not w.nodes[1].location.valid()
-
-    h = Handler()
-    diffs = svr.collect_diffs(100, 100000)
-    assert diffs is not None
-
-    diffs.reader.apply(h, idx="flex_mem")
-
-    assert h.counts == [1, 1, 0, 0]
-
 
 def test_get_newest_change_from_file(tmp_path):
     fn = tmp_path / 'change.opl'
@@ -337,3 +57,297 @@ def test_get_newest_change_from_file(tmp_path):
 
     val = osmium.replication.newest_change_from_file(str(fn))
     assert val == mkdate(2018, 10, 29, 3, 56, 7)
+
+
+class TestReplication:
+
+    @pytest.fixture(params=["requests", "urllib"], autouse=True)
+    def setup_mocks(self, request, monkeypatch):
+        self.url_requests = []
+        self.url_exception = None
+        if request.param == "requests":
+            # Default use of the requests library. Simply patch the get method.
+            def mock_get(*args, **kwargs):
+                if self.url_exception is not None:
+                    raise self.url_exception
+                assert self.url_requests
+                return RequestsResponses(self.url_requests.pop(0))
+            monkeypatch.setattr(osmium.replication.server.requests.Session, "get", mock_get)
+
+            def mk_server(*args, **kwargs):
+                return rserv.ReplicationServer(*args, **kwargs)
+            self.mk_replication_server = mk_server
+
+        elif request.param == "urllib":
+            def mock_get(*args, **kwargs):
+                if self.url_exception is not None:
+                    raise self.url_exception
+                assert self.url_requests
+                return BytesIO(self.url_requests.pop(0))
+            def mk_server(*args, **kwargs):
+                server = rserv.ReplicationServer(*args, **kwargs)
+                server.open_url = mock_get
+                return server
+            self.mk_replication_server = mk_server
+
+
+    def set_result(self, content):
+        self.url_requests = [dedent(content).encode()]
+
+
+    def set_script(self, files):
+        self.url_requests = [dedent(s).encode() for s in files]
+
+
+    def test_get_state_valid(self):
+        self.set_result("""\
+            #Sat Aug 26 11:04:04 UTC 2017
+            txnMaxQueried=1219304113
+            sequenceNumber=2594669
+            timestamp=2017-08-26T11\\:04\\:02Z
+            txnReadyList=
+            txnMax=1219304113
+            txnActiveList=1219303583,1219304054,1219304104""")
+
+        res = self.mk_replication_server("https://test.io").get_state_info()
+
+        assert res is not None
+        assert res.timestamp == mkdate(2017, 8, 26, 11, 4, 2)
+        assert res.sequence == 2594669
+
+
+    def test_get_state_sequence_cut(self):
+        self.set_script(("""\
+            #Sat Aug 26 11:04:04 UTC 2017
+            txnMaxQueried=1219304113
+            sequenceNumber=259""",
+            """\
+            #Sat Aug 26 11:04:04 UTC 2017
+            txnMaxQueried=1219304113
+            sequenceNumber=2594669
+            timestamp=2017-08-26T11\\:04\\:02Z"""))
+
+        res = self.mk_replication_server("https://test.io").get_state_info()
+
+        assert res is not None
+        assert res.timestamp == mkdate(2017, 8, 26, 11, 4, 2)
+        assert res.sequence == 2594669
+
+
+    def test_get_state_date_cut(self):
+        self.set_script(("""\
+            #Sat Aug 26 11:04:04 UTC 2017
+            txnMaxQueried=1219304113
+            sequenceNumber=2594669
+            timestamp=2017-08-2""",
+            """\
+            #Sat Aug 26 11:04:04 UTC 2017
+            txnMaxQueried=1219304113
+            sequenceNumber=2594669
+            timestamp=2017-08-26T11\\:04\\:02Z"""))
+
+        res = self.mk_replication_server("https://test.io").get_state_info()
+
+        assert res is not None
+        assert res.timestamp == mkdate(2017, 8, 26, 11, 4, 2)
+        assert res.sequence == 2594669
+
+
+    def test_get_state_timestamp_cut(self):
+        self.set_script(("""\
+            #Sat Aug 26 11:04:04 UTC 2017
+            txnMaxQueried=1219304113
+            sequenceNumber=2594669
+            timestamp=""",
+            """\
+            #Sat Aug 26 11:04:04 UTC 2017
+            txnMaxQueried=1219304113
+            sequenceNumber=2594669
+            timestamp=2017-08-26T11\\:04\\:02Z"""))
+
+        res = self.mk_replication_server("https://test.io").get_state_info()
+
+        assert res is not None
+        assert res.timestamp == mkdate(2017, 8, 26, 11, 4, 2)
+        assert res.sequence == 2594669
+
+
+    def test_get_state_too_many_retries(self):
+        self.set_script(("""\
+            #Sat Aug 26 11:04:04 UTC 2017
+            txnMaxQueried=1219304113
+            sequenceNumber=2594669
+            timestamp=""",
+            """\
+            #Sat Aug 26 11:04:04 UTC 2017
+            txnMaxQueried=1219304113
+            sequenceNumber=2594669
+            timestamp=""",
+            """\
+            #Sat Aug 26 11:04:04 UTC 2017
+            txnMaxQueried=1219304113
+            sequenceNumber=2594669
+            timestamp=""",
+            """\
+            #Sat Aug 26 11:04:04 UTC 2017
+            txnMaxQueried=1219304113
+            sequenceNumber=2594669
+            timestamp=2017-08-26T11\\:04\\:02Z"""))
+
+        res = self.mk_replication_server("https://test.io").get_state_info()
+
+        assert res is None
+
+
+    def test_get_state_server_timeout(self):
+        self.url_exception = URLError(reason='Mock')
+
+        svr = self.mk_replication_server("https://test.io")
+        assert svr.get_state_info() is None
+
+
+    def test_apply_diffs_count(self):
+        self.set_script(("""\
+            sequenceNumber=100
+            timestamp=2017-08-26T11\\:04\\:02Z
+        """, """
+            n1
+            w1
+            r1
+        """))
+        svr = self.mk_replication_server("https://test.io", "opl")
+
+        h = CountingHandler()
+        assert 100 == svr.apply_diffs(h, 100, 10000)
+
+        assert h.counts == [1, 1, 1, 0]
+
+
+    def test_apply_diffs_without_simplify(self):
+        self.set_script(("""\
+            sequenceNumber=100
+            timestamp=2017-08-26T11\\:04\\:02Z
+        """, """
+            n1 v23
+            n1 v24
+            w1
+            r1
+        """))
+        svr = self.mk_replication_server("https://test.io", "opl")
+
+        h = CountingHandler()
+        assert 100 == svr.apply_diffs(h, 100, 10000, simplify=False)
+        assert [2, 1, 1, 0] == h.counts
+
+
+    def test_apply_diffs_with_simplify(self):
+        self.set_script(("""\
+            sequenceNumber=100
+            timestamp=2017-08-26T11\\:04\\:02Z
+        """, """
+            n1 v23
+            n1 v24
+            w1
+            r1
+        """))
+        svr = self.mk_replication_server("https://test.io", "opl")
+
+        h = CountingHandler()
+        assert 100 == svr.apply_diffs(h, 100, 10000, simplify=True)
+        assert [1, 1, 1, 0] == h.counts
+
+
+    def test_apply_with_location(self):
+        self.set_script(("""\
+            sequenceNumber=100
+            timestamp=2017-08-26T11\\:04\\:02Z
+        """, """
+            n1 x10.0 y23.0
+            w1 Nn1,n2
+        """))
+        svr = self.mk_replication_server("https://test.io", "opl")
+
+        class Handler(CountingHandler):
+            def way(self, w):
+                self.counts[1] += 1
+                assert 2 == len(w.nodes)
+                assert 1 == w.nodes[0].ref
+                assert 10 == w.nodes[0].location.lon
+                assert 23 == w.nodes[0].location.lat
+                assert 2 == w.nodes[1].ref
+                assert not w.nodes[1].location.valid()
+
+        h = Handler()
+        assert 100 == svr.apply_diffs(h, 100, 10000, idx="flex_mem")
+
+        assert h.counts == [1, 1, 0, 0]
+
+
+    def test_apply_reader_without_simplify(self):
+        self.set_script(("""\
+            sequenceNumber=100
+            timestamp=2017-08-26T11\\:04\\:02Z
+        """, """
+            n1 v23
+            n1 v24
+            w1
+            r1
+        """))
+        svr = self.mk_replication_server("https://test.io", "opl")
+
+        h = CountingHandler()
+
+        diffs = svr.collect_diffs(100, 100000)
+        assert diffs is not None
+
+        diffs.reader.apply(h, simplify=False)
+        assert [2, 1, 1, 0] == h.counts
+
+
+    def test_apply_reader_with_simplify(self):
+        self.set_script(("""\
+            sequenceNumber=100
+            timestamp=2017-08-26T11\\:04\\:02Z
+        """, """
+            n1 v23
+            n1 v24
+            w1
+            r1
+        """))
+        svr = self.mk_replication_server("https://test.io", "opl")
+
+        h = CountingHandler()
+        diffs = svr.collect_diffs(100, 100000)
+        assert diffs is not None
+
+        diffs.reader.apply(h, simplify=True)
+        assert [1, 1, 1, 0] == h.counts
+
+
+    def test_apply_reader_with_location(self):
+        self.set_script(("""\
+            sequenceNumber=100
+            timestamp=2017-08-26T11\\:04\\:02Z
+        """, """
+            n1 x10.0 y23.0
+            w1 Nn1,n2
+        """))
+        svr = self.mk_replication_server("https://test.io", "opl")
+
+        class Handler(CountingHandler):
+            def way(self, w):
+                self.counts[1] += 1
+                assert 2 == len(w.nodes)
+                assert 1 == w.nodes[0].ref
+                assert 10 == w.nodes[0].location.lon
+                assert 23 == w.nodes[0].location.lat
+                assert 2 == w.nodes[1].ref
+                assert not w.nodes[1].location.valid()
+
+        h = Handler()
+        diffs = svr.collect_diffs(100, 100000)
+        assert diffs is not None
+
+        diffs.reader.apply(h, idx="flex_mem")
+
+        assert h.counts == [1, 1, 0, 0]
