b54d5cb1c5825fdf08fdb410f651e603806c4c3c
Sarah Hoffmann <lonvia@denofr.de>
2022-09-12 23:32:59 +0200

    add typing for helper callback creator

--- src/osmium/helper.py
+++ src/osmium/helper.py
@@ -1,6 +1,17 @@
-from osmium._osmium import SimpleHandler
+from typing import Optional, Callable, TypeVar
 
-def make_simple_handler(node=None, way=None, relation=None, area=None, changeset=None):
+from osmium._osmium import SimpleHandler
+from osmium.osm import Node, Way, Relation, Area, Changeset
+
+T = TypeVar('T')
+HandlerFunc = Optional[Callable[[T], None]]
+
+
+def make_simple_handler(node: HandlerFunc[Node] = None,
+                        way: HandlerFunc[Way] = None,
+                        relation: HandlerFunc[Relation] = None,
+                        area: HandlerFunc[Area] = None,
+                        changeset: HandlerFunc[Changeset] = None) -> SimpleHandler:
     """ Convenience function that creates a `SimpleHandler` from a set of
         callback functions. Each of the parameters takes an optional callable
         that must expect a single positional parameter with the object being
@@ -10,14 +21,14 @@ def make_simple_handler(node=None, way=None, relation=None, area=None, changeset
         pass
 
     if node is not None:
-        __HandlerWithCallbacks.node = staticmethod(node)
+        setattr(__HandlerWithCallbacks, "node", staticmethod(node))
     if way is not None:
-        __HandlerWithCallbacks.way = staticmethod(way)
+        setattr(__HandlerWithCallbacks, "way", staticmethod(way))
     if relation is not None:
-        __HandlerWithCallbacks.relation = staticmethod(relation)
+        setattr(__HandlerWithCallbacks, "relation", staticmethod(relation))
     if area is not None:
-        __HandlerWithCallbacks.area = staticmethod(area)
+        setattr(__HandlerWithCallbacks, "area", staticmethod(area))
     if changeset is not None:
-        __HandlerWithCallbacks.changeset = staticmethod(changeset)
+        setattr(__HandlerWithCallbacks, "changeset", staticmethod(changeset))
 
     return __HandlerWithCallbacks()
