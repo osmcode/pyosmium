bbcedd39d8cff0276a81bf4f39c0cd9cfc056d99
Sarah Hoffmann <lonvia@denofr.de>
2023-01-20 21:14:10 +0100

    add copyright header

--- src/osmium/osm/__init__.py
+++ src/osmium/osm/__init__.py
@@ -1,3 +1,9 @@
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# This file is part of pyosmium. (https://osmcode.org/pyosmium/)
+#
+# Copyright (C) 2023 Sarah Hoffmann <lonvia@denofr.de> and others.
+# For a full list of authors see the git log.
 
 from osmium.osm.mutable import create_mutable_node, create_mutable_way, create_mutable_relation
 
--- src/osmium/osm/mutable.py
+++ src/osmium/osm/mutable.py
@@ -1,3 +1,9 @@
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# This file is part of pyosmium. (https://osmcode.org/pyosmium/)
+#
+# Copyright (C) 2023 Sarah Hoffmann <lonvia@denofr.de> and others.
+# For a full list of authors see the git log.
 from typing import Optional, Union, Any, Mapping, Sequence, Tuple, TYPE_CHECKING
 from datetime import datetime
 
