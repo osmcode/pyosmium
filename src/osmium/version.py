135801d1037fbaa9ce106840c49044c1dc184076
Sarah Hoffmann <lonvia@denofr.de>
2023-07-15 19:57:10 +0200

    update shipped pybind to 2.11

--- src/osmium/version.py
+++ src/osmium/version.py
@@ -11,11 +11,11 @@ Version information.
 # the major version
 pyosmium_major = '3.6'
 # current release (Pip version)
-pyosmium_release = '3.6.0'
+pyosmium_release = '3.6.1'
 
 # libosmium version shipped with the Pip release
 libosmium_version = '2.19.0'
 # protozero version shipped with the Pip release
 protozero_version = '1.7.1'
 # pybind11 version shipped with the Pip release
-pybind11_version = '2.10.3'
+pybind11_version = '2.11.0'
