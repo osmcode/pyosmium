df5fb8172e0b50f70e471845105276ec61a12208
Sarah Hoffmann <lonvia@denofr.de>
2023-11-18 17:38:30 +0100

    move pyosmium-get_changes tests to pytest-httpserver

--- test/test_pyosmium_get_changes.py
+++ test/test_pyosmium_get_changes.py
@@ -2,10 +2,9 @@
 #
 # This file is part of Pyosmium.
 #
-# Copyright (C) 2022 Sarah Hoffmann.
+# Copyright (C) 2023 Sarah Hoffmann.
 """ Tests for the pyosmium-get-changes script.
 """
-from io import BytesIO
 from pathlib import Path
 from textwrap import dedent
 
@@ -18,101 +17,76 @@ except ImportError:
     import cookielib as cookiejarlib
 
 
-class RequestsResponses(BytesIO):
-
-    def __init__(self, bytes):
-       super(RequestsResponses, self).__init__(bytes)
-       self.content = bytes
-
-    def iter_lines(self):
-       return self.readlines()
-
-
 class TestPyosmiumGetChanges:
 
     @pytest.fixture(autouse=True)
-    def setUp(self, monkeypatch):
+    def setup(self):
         self.script = dict()
 
         filename = (Path(__file__) / ".." / ".." / "tools"/ "pyosmium-get-changes").resolve()
         with filename.open("rb") as f:
             exec(compile(f.read(), str(filename), 'exec'), self.script)
 
-        self.urls = dict()
+
+    def main(self, httpserver, *args):
+        return self.script['main'](['--server', httpserver.url_for('')] + list(args))
 
 
-    @pytest.fixture
-    def mock_requests(self, monkeypatch):
-        def mock_get(session, url, **kwargs):
-            return RequestsResponses(self.urls[url])
-        monkeypatch.setattr(osmium.replication.server.requests.Session, "get", mock_get)
-
-
-    def url(self, url, result):
-        self.urls[url] = dedent(result).encode()
-
-    def main(self, *args):
-        return self.script['main'](args)
-
-
-    def test_init_id(self, capsys):
-        assert 0 == self.main('-I', '453')
+    def test_init_id(self, capsys, httpserver):
+        assert 0 == self.main(httpserver, '-I', '453')
 
         output = capsys.readouterr().out.strip()
 
         assert output == '453'
 
 
-    def test_init_date(self, capsys, mock_requests):
-        self.url('https://planet.osm.org/replication/minute//state.txt',
-                 """\
+    def test_init_date(self, capsys, httpserver):
+        httpserver.expect_request('/state.txt').respond_with_data(dedent("""\
                     sequenceNumber=100
                     timestamp=2017-08-26T11\\:04\\:02Z
-                 """)
-        self.url('https://planet.osm.org/replication/minute//000/000/000.state.txt',
-                 """\
+                 """))
+        httpserver.expect_request('/000/000/000.state.txt').respond_with_data(dedent("""\
                     sequenceNumber=0
                     timestamp=2016-08-26T11\\:04\\:02Z
-                 """)
-        assert 0 == self.main('-D', '2015-12-24T08:08:08Z')
+                 """))
+        assert 0 == self.main(httpserver, '-D', '2015-12-24T08:08:08Z')
 
         output = capsys.readouterr().out.strip()
 
         assert output == '-1'
 
 
-    def test_init_to_file(self, tmp_path):
+    def test_init_to_file(self, tmp_path, httpserver):
         fname = tmp_path / 'db.seq'
 
-        assert 0 == self.main('-I', '453', '-f', str(fname))
+        assert 0 == self.main(httpserver, '-I', '453', '-f', str(fname))
         assert fname.read_text() == '453'
 
 
-    def test_init_from_seq_file(self, tmp_path):
+    def test_init_from_seq_file(self, tmp_path, httpserver):
         fname = tmp_path / 'db.seq'
         fname.write_text('453')
 
-        assert 0 == self.main('-f', str(fname))
+        assert 0 == self.main(httpserver, '-f', str(fname))
         assert fname.read_text() == '453'
 
 
-    def test_init_date_with_cookie(self, capsys, tmp_path, mock_requests):
-        self.url('https://planet.osm.org/replication/minute//state.txt',
-                 """\
+    def test_init_date_with_cookie(self, capsys, tmp_path, httpserver):
+        httpserver.expect_request('/state.txt').respond_with_data(dedent("""\
                     sequenceNumber=100
                     timestamp=2017-08-26T11\\:04\\:02Z
-                 """)
-        self.url('https://planet.osm.org/replication/minute//000/000/000.state.txt',
-                 """\
+                 """))
+        httpserver.expect_request('/000/000/000.state.txt').respond_with_data(dedent("""\
                     sequenceNumber=0
                     timestamp=2016-08-26T11\\:04\\:02Z
-                 """)
+                 """))
 
         fname = tmp_path / 'my.cookie'
         cookie_jar = cookiejarlib.MozillaCookieJar(str(fname))
         cookie_jar.save()
 
-        assert 0 == self.main('--cookie', str(fname), '-D', '2015-12-24T08:08:08Z')
+        assert 0 == self.main(httpserver, '--cookie', str(fname),
+                              '-D', '2015-12-24T08:08:08Z')
 
         output = capsys.readouterr().out.strip()
 
