df82c95f9008c978b5c73d86dfc662e3b47b4cfd
Sarah Hoffmann <lonvia@denofr.de>
2024-03-03 21:38:18 +0100

    allow arbitrary number of handlers in apply

--- lib/osmium.cc
+++ lib/osmium.cc
@@ -5,14 +5,73 @@
  * Copyright (C) 2024 Sarah Hoffmann <lonvia@denofr.de> and others.
  * For a full list of authors see the git log.
  */
+#include <vector>
+
 #include <pybind11/pybind11.h>
 
 #include <osmium/osm.hpp>
+#include <osmium/handler.hpp>
 
 #include "simple_handler.h"
 #include "osmium_module.h"
 
 namespace py = pybind11;
+
+class HandlerChain : public osmium::handler::Handler
+{
+public:
+    HandlerChain(std::vector<BaseHandler *> &&handlers)
+    : m_handlers(handlers)
+    {}
+
+    void node(osmium::Node const &o) {
+        for (auto const &handler : m_handlers) {
+            handler->node(&o);
+        }
+    }
+
+    void way(osmium::Way &w) {
+        for (auto const &handler : m_handlers) {
+            handler->way(&w);
+        }
+    }
+
+    void relation(osmium::Relation const &o) {
+        for (auto const &handler : m_handlers) {
+            handler->relation(&o);
+        }
+    }
+
+    void changeset(osmium::Changeset const &o) {
+        for (auto const &handler : m_handlers) {
+            handler->changeset(&o);
+        }
+    }
+
+    void area(osmium::Area const &o) {
+        for (auto const &handler : m_handlers) {
+            handler->area(&o);
+        }
+    }
+
+private:
+    std::vector<BaseHandler *> m_handlers;
+};
+
+
+static HandlerChain make_handler_chain(py::args args)
+{
+    std::vector<BaseHandler *> handlers;
+    for (auto const &arg: args) {
+        if (py::isinstance<BaseHandler>(arg)) {
+            handlers.push_back(arg.cast<BaseHandler *>());
+        } else {
+            throw py::type_error{"Argument must be a handler-like object."};
+        }
+    }
+
+    return HandlerChain(std::move(handlers));
+}
 
 PYBIND11_MODULE(_osmium, m) {
     py::register_exception<osmium::invalid_location>(m, "InvalidLocationError");
@@ -27,10 +86,16 @@ PYBIND11_MODULE(_osmium, m) {
     m.def("apply", [](osmium::io::Reader &rd, BaseHandler &h)
                    { py::gil_scoped_release release; osmium::apply(rd, h); },
           py::arg("reader"), py::arg("handler"),
-          "Apply a chain of handlers.");
-    m.def("apply", [](osmium::io::Reader &rd, BaseHandler &b1, BaseHandler &b2)
-                     { py::gil_scoped_release release; osmium::apply(rd, b1, b2); },
-          py::arg("reader"), py::arg("handler1"), py::arg("handler2"),
+          "Apply a single handler.");
+    m.def("apply", [](osmium::io::Reader &rd, py::args args)
+                     {
+                         auto handler = make_handler_chain(args);
+                         {
+                             py::gil_scoped_release release;
+                             osmium::apply(rd, handler);
+                         }
+                     },
+          py::arg("reader"),
           "Apply a chain of handlers.");
 
     py::class_<BaseHandler>(m, "BaseHandler");
